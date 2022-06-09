97b431a7103e4c8107b72a5ac4c9dacdb0f22b8c
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-09 11:22:55 +0200

    update 2.0

--- script.js
+++ script.js
@@ -6,8 +6,12 @@ let currentShape = 'cross';
 function fillShape(id) {
     if (currentShape == 'cross') {
         currentShape = 'circle';
+        document.getElementById('player-1').classList.remove('player-inactiv');
+        document.getElementById('player-2').classList.add('player-inactiv');
     } else {
         currentShape = 'cross';
+        document.getElementById('player-2').classList.remove('player-inactiv');
+        document.getElementById('player-1').classList.add('player-inactiv');
     }
     storage[id] = currentShape;
     console.log(storage);
@@ -24,4 +28,37 @@ function draw() {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
     }
+}
+
+function checkWinner() {
+    let winner;
+
+    if (storage[0] == storage[1] && storage[1] == storage[2] && storage[0]) {
+        winner = storage[0];
+    }
+    if (storage[3] == storage[4] && storage[4] == storage[5] && storage[3]) {
+        winner = storage[3];
+    }
+    if (storage[6] == storage[7] && storage[7] == storage[8] && storage[6]) {
+        winner = storage[6];
+    }
+    if (storage[0] == storage[3] && storage[3] == storage[6] && storage[0]) {
+        winner = storage[0];
+    }
+    if (storage[1] == storage[4] && storage[4] == storage[7] && storage[1]) {
+        winner = storage[1];
+    }
+    if (storage[2] == storage[5] && storage[5] == storage[8] && storage[2]) {
+        winner = storage[2];
+    }
+    if (storage[0] == storage[4] && storage[4] == storage[8] && storage[0]) {
+        winner = storage[0];
+    }
+    if (storage[2] == storage[4] && storage[4] == storage[6] && storage[2]) {
+        winner = storage[2];
+    }
+    if (!!winner) {
+        console.log('Gewonnen:', winner);
+    }
+
 }
