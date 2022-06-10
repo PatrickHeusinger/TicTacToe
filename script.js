c01ee2e1f87867a5cfb7b1c37e167b41ed481759
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-06-10 15:20:34 +0200

    reset

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function fillShape(id) {
         storage[id] = currentShape;
         draw();
         checkWinner();
+
     }
 
 }
@@ -39,34 +40,42 @@ function checkWinner() {
     if (storage[0] == storage[1] && storage[1] == storage[2] && storage[0]) {
         winner = storage[0];
         document.getElementById('line-1').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-1').classList.remove('d-none');
     }
     if (storage[3] == storage[4] && storage[4] == storage[5] && storage[3]) {
         winner = storage[3];
         document.getElementById('line-2').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-2').classList.remove('d-none');
     }
     if (storage[6] == storage[7] && storage[7] == storage[8] && storage[6]) {
         winner = storage[6];
         document.getElementById('line-3').style.transform = 'scaleX(1.0)';
+        document.getElementById('line-3').classList.remove('d-none');
     }
     if (storage[0] == storage[3] && storage[3] == storage[6] && storage[0]) {
         winner = storage[0];
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-4').classList.remove('d-none');
     }
     if (storage[1] == storage[4] && storage[4] == storage[7] && storage[1]) {
         winner = storage[1];
         document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-5').classList.remove('d-none');
     }
     if (storage[2] == storage[5] && storage[5] == storage[8] && storage[2]) {
         winner = storage[2];
         document.getElementById('line-6').style.transform = 'rotate(90deg) scaleX(1.0)';
+        document.getElementById('line-6').classList.remove('d-none');
     }
     if (storage[0] == storage[4] && storage[4] == storage[8] && storage[0]) {
         winner = storage[0];
         document.getElementById('line-7').style.transform = 'rotate(45deg) scaleX(1.0)';
+        document.getElementById('line-7').classList.remove('d-none');
     }
     if (storage[2] == storage[4] && storage[4] == storage[6] && storage[2]) {
         winner = storage[2];
         document.getElementById('line-8').style.transform = 'rotate(-45deg) scaleX(1.0)';
+        document.getElementById('line-8').classList.remove('d-none');
     }
     if (!!winner) {
         gameOver = true;
